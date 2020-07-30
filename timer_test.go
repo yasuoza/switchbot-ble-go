1adc0a16a895801e1151543fb7f83614fa615a4e
Yasuharu Ozaki <yasuharu.ozaki@gmail.com>
2020-07-31 00:04:42 +0900

    Prefer table driven test

--- timer_test.go
+++ timer_test.go
@@ -30,120 +30,83 @@ func TestParseTimerReturnsNil(t *testing.T) {
 	}
 }
 
-func TestParseTimerPressRepeatEnabled(t *testing.T) {
-	// - Enabled
-	// - Repeat Sunday, Monday, Thursday, Friday, Saturday
-	// - Hour 10
-	// - Minutes 11
-	d := []byte{1, 2, 0, 121, 10, 11, 0, 0, 0, 0, 0, 0}
+func TestParseTimer(t *testing.T) {
+	type Want struct {
+		enabled  bool
+		weekdays [7]bool
+		hour     int
+		minutes  int
+		action   int
+	}
+	tests := []struct {
+		got  []byte
+		want Want
+	}{
+		{
+			got: []byte{1, 2, 0, 121, 10, 11, 0, 0, 0, 0, 0, 0},
+			want: Want{
+				enabled:  true,
+				weekdays: [7]bool{true, true, false, false, true, true, true},
+				hour:     10,
+				minutes:  11,
+				action:   0,
+			},
+		},
+		{
+			got: []byte{1, 2, 0, 0, 12, 0, 64, 0, 0, 0, 0, 0},
+			want: Want{
+				enabled:  false,
+				weekdays: [7]bool{true, false, false, false, false, false, false},
+				hour:     12,
+				minutes:  0,
+				action:   0,
+			},
+		},
+		{
+			got: []byte{1, 3, 0, 31, 10, 11, 0, 1, 0, 0, 0, 0},
+			want: Want{
+				enabled:  true,
+				weekdays: [7]bool{false, true, true, true, true, true, false},
+				hour:     10,
+				minutes:  11,
+				action:   1,
+			},
+		},
+		{
+			got: []byte{1, 3, 0, 4, 15, 33, 0, 2, 0, 0, 0, 0},
+			want: Want{
+				enabled:  true,
+				weekdays: [7]bool{false, false, false, true, false, false, false},
+				hour:     15,
+				minutes:  33,
+				action:   2,
+			},
+		},
+		{
+			got: []byte{1, 2, 0, 128, 18, 45, 0, 2, 0, 0, 0, 0},
+			want: Want{
+				enabled:  true,
+				weekdays: [7]bool{false, false, false, false, false, false, false},
+				hour:     18,
+				minutes:  45,
+				action:   2,
+			},
+		},
+		{
+			got: []byte{1, 2, 0, 0, 18, 45, 128, 2, 0, 0, 0, 0},
+			want: Want{
+				enabled:  false,
+				weekdays: [7]bool{false, false, false, false, false, false, false},
+				hour:     18,
+				minutes:  45,
+				action:   2,
+			},
+		},
+	}
 
-	timer := ParseTimerBytes(d)
-	testTimer(
-		t,
-		timer,
-		true,
-		[7]bool{true, true, false, false, true, true, true},
-		10,
-		11,
-		0,
-	)
-}
-
-func TestParseTimerBytesPressRepeatDisabled(t *testing.T) {
-	// - Disabled
-	// - Repeat Sunday
-	// - Hour 12
-	// - Minutes 00
-	d := []byte{1, 2, 0, 0, 12, 0, 64, 0, 0, 0, 0, 0}
-
-	timer := ParseTimerBytes(d)
-	testTimer(
-		t,
-		timer,
-		false,
-		[7]bool{true, false, false, false, false, false, false},
-		12,
-		0,
-		0,
-	)
-}
-
-func TestParseTimerBytesOnRepeatEnabled(t *testing.T) {
-	// - Enabled
-	// - Repeat Monday, Tuesday, Wednesday, Thursday, Friday
-	// - Hour 10
-	// - Minutes 11
-	// - On
-	d := []byte{1, 3, 0, 31, 10, 11, 0, 1, 0, 0, 0, 0}
-
-	timer := ParseTimerBytes(d)
-	testTimer(
-		t,
-		timer,
-		true,
-		[7]bool{false, true, true, true, true, true, false},
-		10,
-		11,
-		1,
-	)
-}
-
-func TestParseTimerBytesOffRepeastEnabled(t *testing.T) {
-	// - Enabled
-	// - Repeat Wednesday
-	// - Hour 15
-	// - Minutes 33
-	// - Off
-	d := []byte{1, 3, 0, 4, 15, 33, 0, 2, 0, 0, 0, 0}
-
-	timer := ParseTimerBytes(d)
-	testTimer(
-		t,
-		timer,
-		true,
-		[7]bool{false, false, false, true, false, false, false},
-		15,
-		33,
-		2,
-	)
-}
-
-func TestParseTimerBytesOffOnceEnabled(t *testing.T) {
-	// - Enabled
-	// - Once
-	// - Hour 18
-	// - Minutes 45
-	// - Off
-	d := []byte{1, 2, 0, 128, 18, 45, 0, 2, 0, 0, 0, 0}
-
-	timer := ParseTimerBytes(d)
-	testTimer(
-		t,
-		timer,
-		true,
-		[7]bool{false, false, false, false, false, false, false},
-		18,
-		45,
-		2,
-	)
-}
-
-func TestParseTimerBytesOffOnceDisabled(t *testing.T) {
-	// - Disabled
-	// - Once
-	// - Hour 18
-	// - Minutes 45
-	// - Off
-	d := []byte{1, 2, 0, 0, 18, 45, 128, 2, 0, 0, 0, 0}
-
-	timer := ParseTimerBytes(d)
-	testTimer(
-		t,
-		timer,
-		false,
-		[7]bool{false, false, false, false, false, false, false},
-		18,
-		45,
-		2,
-	)
+	for _, tt := range tests {
+		got, want := tt.got, tt.want
+		timer := ParseTimerBytes(got)
+		testTimer(t, timer, want.enabled, want.weekdays, want.hour, want.minutes, want.action)
+	}
 }
