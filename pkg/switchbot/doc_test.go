c9e6868870cd5aaa343ccdc19a1b6ca48b1fe4c1
Yasuharu Ozaki <yasuoza@users.noreply.github.com>
2021-11-12 01:09:35 +0900

    Update docs

--- pkg/switchbot/doc_test.go
+++ pkg/switchbot/doc_test.go
@@ -3,6 +3,7 @@ package switchbot_test
 import (
 	"context"
 	"log"
+	"os"
 	"time"
 
 	"github.com/yasuoza/switchbot-ble-go/v2/pkg/switchbot"
@@ -12,8 +13,23 @@ func Example_press() {
 	ctx := context.Background()
 	timeout := 5 * time.Second
 
+	// Scan SwitchBots.
+	var addrs []string
+	err := switchbot.Scan(ctx, timeout, func(addr string) {
+		addrs = append(addrs, addr)
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If there is no SwitchBot, err is nil and length of addresses is 0.
+	if len(addrs) == 0 {
+		log.Println("SwitchBot not found")
+		os.Exit(0)
+	}
+
 	// First, connect to SwitchBot.
-	addr := "A0:00:0A:AA:00:00"
+	addr := addrs[0]
 	log.Printf("Connecting to SwitchBot %s\n", addr)
 	bot, err := switchbot.Connect(ctx, addr, timeout)
 	if err != nil {
